0a46baa5bcafb3d22a085a1c772de8835ca6a544
AnveshAnnepaga <annepagaanvesh44@gmail.com>
2026-02-18 20:56:37 +0530

    Updated Readme.md

--- milestone4/UI/UI/components/sidebar.py
+++ milestone4/UI/UI/components/sidebar.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from services.history import delete_run, get_run, list_runs
+from milestone4.UI.UI.services.history import delete_run, get_run, list_runs
 
 def render_sidebar():
     # 🔥 No more boring user details here. 
